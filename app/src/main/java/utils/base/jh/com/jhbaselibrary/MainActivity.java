af9a751132f33d5d981b533fe43daeafc11ccef5
nnzhong <nnzhong@joinhealth.cn>
2018-02-27 09:53:06 +0800

    111

---
+++ app/src/main/java/utils/base/jh/com/jhbaselibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package utils.base.jh.com.jhbaselibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
